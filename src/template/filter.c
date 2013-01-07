7ab4f501908bad10ae194c6c805718ea97ab2247
John Hodge <tpg@mutabah.net>
2013-01-07 22:05:22 +0800

    Templates - Forgotten file

---
+++ src/template/filter.c
@@ -0,0 +1,42 @@
+/*
+ * 
+ */
+#include "template_common.h"
+#include <string.h>
+#include <spiderscript.h>
+
+// === CODE ===
+void *Template_int_CreateFilterString(t_obj_Template *State, const char *InputString)
+{
+	return SpiderScript_CreateString(strlen(InputString), InputString);
+}
+
+void Template_int_FreeFilterString(t_obj_Template *State, void *String)
+{
+	SpiderScript_DereferenceString(String);
+}
+
+int Template_int_FilterString(t_obj_Template *State, void **StringPtr, const char *FilterName)
+{
+	tSpiderString	*retval;
+	 int	argtypes[] = {SS_DATATYPE_STRING};
+	const void	*args[] = {*StringPtr};
+	 int	rv = SpiderScript_ExecuteFunction(State->Script, FilterName, &retval, 1, argtypes, args, NULL);
+	
+	if( rv != SS_DATATYPE_STRING )
+	{
+		// uhh...
+		return -1;
+	}
+
+	SpiderScript_DereferenceString(*StringPtr);
+	*StringPtr = retval;
+	
+	return 0;
+}
+
+void Template_int_GetFilterString(t_obj_Template *State, void *String, const char **DataPtr, size_t *LenPtr)
+{
+	*DataPtr = ((tSpiderString*)String)->Data;
+	*LenPtr = ((tSpiderString*)String)->Length;
+}
